61193c385f00e62f389a8b6a63fb9ff24bb94693
HenriqueBS0 <henrique.10agr@gmail.com>
2022-01-29 18:29:08 -0300

    Authentication control on 'link' routes

--- src/Routes/RoutesLink.php
+++ src/Routes/RoutesLink.php
@@ -3,11 +3,13 @@
 namespace HenriqueBS0\LinkPack\Routes;
 
 use HenriqueBS0\LinkPack\Controllers\ControllerLink;
+use HenriqueBS0\LinkPack\Middlewares\MiddlewareAuthentication;
 use HenriqueBS0\Router\RoutesGroup;
 
 class RoutesLink extends RoutesGroup {
     protected function setRoutes(): void
     {
+        $this->addMiddleware(MiddlewareAuthentication::class);
         $this->group('/link');
 
         $this->get('/', [ControllerLink::class, 'list']);
